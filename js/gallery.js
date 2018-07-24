a5dd341a05ea445971cebf68c45ee8de94d246cc
Hakxel <asiel_montes@yahoo.com>
2018-07-24 11:44:21 -0400

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -3,13 +3,27 @@
 // and respond appropriately on click.
 
 function activateGallery() {
-  let thumbnails = document.querySelectorAll("#gallery-thumbs img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
+      // Set clicked image as display image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
+
+      // Change which image is current.
+      let currentClass = "current"
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
